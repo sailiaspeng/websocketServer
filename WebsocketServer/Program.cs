9dd636ad8e1e66015f8c754c3bbb99719048ff89
Sailias <pengjiangyi@shanghai3h.com>
2017-05-15 14:36:29 +0800

    Check In and Push All Code

---
+++ WebsocketServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Configuration;
+using WebsocketServer;
+using System.Diagnostics;
+ 
+namespace WebSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSocket socket = new WebSocket();
+            LogListener listener = new LogListener();
+            Trace.Listeners.Add(listener);
+            try
+            {
+                string ip = ConfigurationManager.AppSettings["serverAddress"];
+                int port = int.Parse(ConfigurationManager.AppSettings["serverPort"]);
+                socket.start(ip, port);
+            }
+            catch (Exception ex) {
+                Trace.TraceInformation("消息服务器启动失败！" + ex.Message);
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
